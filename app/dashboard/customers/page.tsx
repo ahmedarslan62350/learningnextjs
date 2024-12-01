e927384f0e498ff0ff8ed013b25669cd02cbc13a
ahmedarslan62350 <ahmedarslana62350@gmail.com>
2024-12-01 18:28:55 +0500

    project 75% completed

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
-  return (
-    <div>Customers</div>
-  )
-}
+  return <div>page</div>;
+};
 
-export default page
+export default page;
